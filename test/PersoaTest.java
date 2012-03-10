8d92c128618a92190a92bdcff131543669c27546
rubendr <rubendramos@gmail.com>
2012-03-10 12:38:52 +0100

    Adding test environment

---
+++ test/PersoaTest.java
@@ -0,0 +1,48 @@
+import org.junit.*;
+import java.util.*;
+import play.test.*;
+import models.*;
+import java.text.DateFormat;
+
+
+public class PersoaTest extends UnitTest {
+
+	 @Before
+	    public void setup() {
+	        Fixtures.deleteAllModels();
+                Fixtures.loadModels("data.yml");
+	    }
+	
+	
+    @Test
+    public void aVeryImportantThingToTest() {
+        assertEquals(2, 1 + 1);
+    }
+    
+    @Test
+    public void createAndRetrievePersoa() {
+        // Create a new Persoa and save it
+        
+       Persoa p=Persoa.find("byDni", "11111111H").first();
+        
+       //Persoa bob = Persoa.find("byDni", "76574900B").first();
+       ComunidadeAutonoma ca=ComunidadeAutonoma.all().first();
+       Provincia pr=Provincia.all().first();
+        
+        //String a=ca.descricion;
+       Sexo s=Sexo.all().first();
+       Enderezo enderezo=Enderezo.all().first();
+        
+        //new Persoa(p.nome,p.apelido1,p.apelido2,p.dni,p.email,s,p.dataNacemento,enderezo).save();
+        
+       
+
+        // Retrieve the Persoa with bob Persoaname
+        
+
+        // Test 
+        //assertNotNull(bob);
+        //assertEquals("diaz ramos, ruben", bob.nomeCompleto);
+    }
+
+}
