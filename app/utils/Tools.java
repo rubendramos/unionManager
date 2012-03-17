6aceb58d356d552db423c500ed0e95d6b1a42ad5
rubendr <rubendramos@gmail.com>
2012-03-17 22:24:57 +0100

    Updating tools

--- app/utils/Tools.java
+++ app/utils/Tools.java
@@ -5,20 +5,52 @@
 package utils;
 
 import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author T00159
  */
 public class Tools {
-    
-    
-    public static String getLocaleDateFormat(Date date){
-                
-        java.text.SimpleDateFormat nf=new java.text.SimpleDateFormat("dd/MM/yyyy");
+
+    public static String getLocaleDateFormat(Date date) {
+
+        java.text.SimpleDateFormat nf = new java.text.SimpleDateFormat("dd/MM/yyyy");
         //DateFormat nf=DateFormat.getDateInstance();
         return nf.format(date);
-        
+
+    }
+
+    public static Date getDataBaseDateFormat(String date) {
+        try {
+            java.text.SimpleDateFormat nf = new java.text.SimpleDateFormat("yyyy-MM-dd");
+            return nf.parse(date);
+        } catch (ParseException ex) {
+            Logger.getLogger(Tools.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+
+    }
+
+    public static Date getAppDateFormat(String date) {
+        try {
+            java.text.SimpleDateFormat nf = new java.text.SimpleDateFormat("dd/MM/yyyy");
+            return nf.parse(date);
+        } catch (ParseException ex) {
+            Logger.getLogger(Tools.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+
+    }
+
+    public static String dateToDateDataBaseFormat(String dateString) {
+
+        java.text.SimpleDateFormat dataBaseFormatDate = new java.text.SimpleDateFormat("yyyy-MM-dd");
+        return dataBaseFormatDate.format(getAppDateFormat(dateString));
+
     }
 }
