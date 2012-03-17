b094005e051b94964e31df5d87921f555c4fc8e9
rubendr <rubendramos@gmail.com>
2012-03-17 22:23:32 +0100

    Update Notificator

--- app/notificacions/Notificador.java
+++ app/notificacions/Notificador.java
@@ -14,16 +14,16 @@ public class Notificador extends Mailer {
         
         
         //asunto do mail
-        setSubject(aviso.asunto);
+        setSubject(aviso.getAsunto());
         
         //Engadimos os destinatarios
-       addRecipient(aviso.getEmailsListasDistribucion());
+       addRecipient("rubendramos@gmail.com");
         
        //Contacto que o envía
        setFrom(new InternetAddress(aviso.avisoDe, aviso.avisoDe));
         
         //Adxuntamos os docs
-        if(aviso.docsAdxuntos!=null){
+        if(aviso.docsAdxuntos!=null && !aviso.docsAdxuntos.isEmpty()){
             for(Documento doc : aviso.docsAdxuntos){
                     EmailAttachment attachment = new EmailAttachment();
                     attachment.setDescription(doc.descricion);
@@ -39,6 +39,22 @@ public class Notificador extends Mailer {
         //Enviase e esperase resposta
         return sendAndWait();
     }
+    
+    public static boolean notificacionSistema(Aviso aviso) throws Exception {
+        
+        
+        //asunto do mail
+        setSubject(aviso.getAsunto());
+        
+        //Engadimos os destinatarios
+       addRecipient("rubendramos@gmail.com");
+        
+       //Contacto que o envía
+       setFrom(new InternetAddress(aviso.avisoDe, aviso.avisoDe));     
+          
+        //Enviase e esperase resposta
+        return sendAndWait(aviso);
+    }    
     
 }
 
